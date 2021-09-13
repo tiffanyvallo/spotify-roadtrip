cd7185b42badf0ff2e1654e4d181c71c2ac77bb0
fwill22 <79596926+fwill22@users.noreply.github.com>
2021-09-13 15:27:49 +0100

    Written axios request - dropdowns not staying on page after initial rendering - need to fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Dropdown from './Dropdown'
-
-const data = [
-  {value: 1, name: 'A'},
-  {value: 2, name: 'B'},
-  {value: 3, name: 'C'}
-]
+import axios from 'axios'
 
 const App = () => {
+
+  console.log('RENDERING APP.JS')
+
+  const data = [
+    {value: 1, name: 'A'},
+    {value: 2, name: 'B'},
+    {value: 3, name: 'C'}
+  ]
+
+  const [token, setToken] = useState('');
+
+  useEffect(() => {
+
+    axios('https://accounts.spotify.com/api/token', {
+      headers: {
+        'Content-Type' : 'application/x-www-form-urlencoded',
+        'Authorization' : 'Basic ' + CLIENT_ID + ':' + CLIENT_SECRET
+      },
+      data: 'grant_type=client_credentials',
+      method: 'POST'
+    })
+    .then(tokenResponse => {
+      console.log(tokenResponse.data.access_token);
+      setToken(tokenResponse.data.access_token);
+    });
+
+  }, []);
+  
   return (
     <form onSubmit={() => {}}>
       <div className="container">
